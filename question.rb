c59fc4054ec2027c112f75aa87dd1c100e58ccbc
Matthew Cook <matthewcook13987@gmail.com>
2021-03-29 17:17:02 +0000

    simplified question logic for easier buidling of app, added new methods to question class to give the question prompt and test the answer

--- question.rb
+++ question.rb
@@ -3,10 +3,24 @@ class Question
   attr_accessor :answer
 
   def initialize
-    @n1 = rand(1..12)
-    @n2 = rand(1..12)
-    @operand = ['+', '-', '/', '*']
-    @question = @n1 @operand @n2
-    @answer = @n1 @operand @n2
+    @num1 = rand(1..12)
+    @num2 = rand(1..12)
+    #@operand = ['+', '-', '/', '*'].sample
+    #puts = "What is #{@n1} #{@operand} #{@n2}"
+    @answer = @num1 + @num2
   end
+
+  def prompt
+    "What is #{@num1} + #{@num2}"
+  end
+
+  def is_correct(user_ans)
+    if user_ans == @answer
+      return true
+    else
+      return false
+    end
+  end
+
+
 end
